6560a6415a8d505f981be0435b65c709e9ea3ba9
Gerald Rotich <58162259+geraldrotich@users.noreply.github.com>
2025-06-21 21:15:47 +0300

    Update login.js code

--- js/login.js
+++ js/login.js
@@ -1,1 +1,12 @@
+function loginUser() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "1234") {
+    window.location.href = "dashboard-products.html";
+  } else {
+    alert("Invalid login");
+  }
+  return false;
+}
 
